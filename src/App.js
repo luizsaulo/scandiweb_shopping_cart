177cbdcb8f6c427ea5ea99de9dfea1d8607a2000
Luiz <luizsaulo@yahoo.com.br>
2022-03-01 14:44:00 -0300

    adding the css style sheet and the firsts configurations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import React from "react";
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Shopping Cart</a>
-      </header>
-      <main>
-        Product list
-      </main>
-      <footer>All rights reserved</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              Products
+            </div>
+            <div className="sidebar">
+              Cart items
+            </div>
+          </div>
+        </main>
+        <footer>All rights reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+          <ul className='products'>
+            {this.props.products.map(product => (
+                <li key={product._id}>
+                    <div className='product'>
+                        <a href='#'>
+                            <img  src={product.image} alt='product image'> </img>
+                            <p>
+                                {product.title}
+                            </p>
+                        </a>
+                        <div className='product-price'>
+                            
+                        </div>
+                    </div>
+                </li>
+            ))}
+          </ul>
+      </div>
+    )
+  }
+}
