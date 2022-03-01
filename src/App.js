b201e52d18ab93ed082149ddfbae559c6a2013ef
Luiz <luizsaulo@yahoo.com.br>
2022-03-01 10:24:54 -0300

    initializing the project for scandiweb test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main>
+        Product list
+      </main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
